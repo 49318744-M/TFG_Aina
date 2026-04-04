7e18fc353a844a13f432dc4a7f7c3b8137ba8329
49318744-M <aina.rovira@estudiants.urv.cat>
2026-04-04 19:42:13 +0200

    main arreglat

--- main.tf
+++ main.tf
@@ -16,11 +16,30 @@ resource "aws_instance" "monai_instance" {
   ami           = "ami-08aa4c8884cdec81f" # Ubuntu 22.04 en us-east-1
   instance_type = "g4dn.xlarge"
   key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.monai_sg.id]
 
   tags = { Name = var.instance_name }
 
   root_block_device {
     volume_size = 40
+  }
+}
+resource "aws_security_group" "monai_sg" {
+  name        = "monai-build-sg"
+  description = "Permetre SSH per al build de MONAI"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permet l'accés des de GitHub Actions
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
