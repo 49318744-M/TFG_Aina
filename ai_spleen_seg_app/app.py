47c2c9297f19d0131c460387033430e9690d2416
49318744-M <aina.rovira@estudiants.urv.cat>
2026-04-04 21:38:42 +0200

    mes2

--- ai_spleen_seg_app/app.py
+++ ai_spleen_seg_app/app.py
@@ -1,8 +1,19 @@
-from monai.deploy.core import Application
+%YAML 1.1
+---
+application: # Aquesta és la clau que el SDK t'està demanant
+  name: spleen_segmentation_app
+  version: 1.0.0
+  description: App per a la segmentació de melsa basada en MONAI Deploy
+  main: app.py
 
-class SpleenApp(Application):
-    def compose(self):
-        pass
+  # Paràmetres del model
+  models:
+    - name: spleen_model
+      model: model.ts
+      format: torchscript
 
-if __name__ == "__main__":
-    SpleenApp().execute()
+  # Definició dels recursos
+  resources:
+    cpu: 1
+    gpu: 1
+    memory: 4Gi
