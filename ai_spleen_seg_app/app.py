3aabee6e0ce47e3875f0f9ee40473fa751c957b9
49318744-M <aina.rovira@estudiants.urv.cat>
2026-04-06 16:56:04 +0200

    fase2

--- ai_spleen_seg_app/app.py
+++ ai_spleen_seg_app/app.py
@@ -1,8 +1,12 @@
-from monai.deploy.core import Application
+from monai.deploy.operators.monai_seg_inference_operator import MonaiSegInferenceOperator
+from monai.deploy.core import Application, resource
 
-class SpleenApp(Application):
+@resource(cpu=1, gpu=1, memory="4Gi")
+class SpleenSegApp(Application):
     def compose(self):
-        pass
+        # El nombre 'spleen_model' debe coincidir con el del app.yml
+        seg_operator = MonaiSegInferenceOperator(model_path="model/model.ts")
+        self.add_operator(seg_operator)
 
 if __name__ == "__main__":
-    SpleenApp().execute()
+    SpleenSegApp().execute()
